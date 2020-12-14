8270209db36cd0d6d4b82d081d7c4d416d4c9afe
Kostas Vs <kostasvs@live.com>
2020-12-14 23:40:58 +0200

    import bootstrap & fontawesome

--- client/main.js
+++ client/main.js
@@ -1,5 +1,8 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
+import 'bootstrap/dist/js/bootstrap.bundle';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
 
 import './main.html';
 
