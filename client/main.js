d2988987edd63561410c30fd5fa37c645c79ef6a
Kostas Vs <kostasvs@live.com>
2020-12-14 23:54:45 +0200

    registration page layout

--- client/main.js
+++ client/main.js
@@ -5,21 +5,23 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
 
 import './main.html';
+import './navbar.html';
+import './register.html';
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
+//Template.hello.onCreated(function helloOnCreated() {
+//  // counter starts at 0
+//  this.counter = new ReactiveVar(0);
+//});
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
+//Template.hello.helpers({
+//  counter() {
+//    return Template.instance().counter.get();
+//  },
+//});
 
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
-});
+//Template.hello.events({
+//  'click button'(event, instance) {
+//    // increment the counter when button is clicked
+//    instance.counter.set(instance.counter.get() + 1);
+//  },
+//});
